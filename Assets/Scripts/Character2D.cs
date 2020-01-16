72c17d70af0b3677df3fb5f242c1519ddc58d93d
luzarce07 <luzarce7@gmail.com>
2020-01-16 07:54:21 -0700

    Funciona axis function y speed

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
-    
+    //Esta propiedad permite que se vea en el inspector siendo private
+    [SerializeField] 
+
+    //Si pongo public se vuelve visible en el inspector de Unity (No es buena idea ponerlo en public)
+    float moveSpeed;
+    //float moveSpeed{get; set;} si lo ponemos asi no se vera en el inspector
+
+
+
     void Update()
     {
         //Se mueve a la derecha muy rapido
-        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
 
     }
 
+    //Funcion devuelve algo, metodos son los void's (esta es la foma correcta de hacer una funcion mas practica)
+    Vector2 Axis
+    {
+
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); //Aqui pones lo que quieres regresar
+    
+    }
+
+    //Encapsulamiento
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+
+
+
+    /* Esta no
+    Vector2 Axis2()
+    {
+        return new Vector2();
+    }
+    */
 
 
 }
